46799b72f87d0921de1eef617b354e062253f198
LuizGustavo <luizgustavo.sp2020@gmail.com>
2024-04-13 09:53:20 -0300

    Criando algoritmos com array e template strings

---
+++ script.js
@@ -0,0 +1,66 @@
+'use strict'
+
+let listaNumeros = [1,2,3,4,5,6,7,8,9]
+let pares = []
+let impares = []
+
+let cont = 0
+
+// while(cont < listaNumeros.length){
+//     const numero = listaNumeros[cont]
+
+//     if(numero % 2 == 0){
+//         pares.push(numero)
+//     }else{
+//         impares.push(numero)
+//     }
+    
+//     cont++
+// }
+
+for(let numero of listaNumeros){
+
+    if(numero % 2 == 0){
+        pares.push(numero)
+    }else{
+        impares.push(numero)
+    }
+}
+
+// listaNumeros.forEach( numero => {
+//     if(numero % 2 == 0){
+//         pares.push(numero)
+//     }else{
+//         impares.push(numero)
+//     }
+// })
+
+console.log(impares);
+console.log(pares);
+
+
+const listaKaratecas = [
+    {
+        nome: "Luizão",
+        notas: [10, 9]
+    },
+    {
+        nome: "Miguel",
+        notas: [8, 9]
+    },
+    {
+        nome: "Falcão",
+        notas: [6.5, 9.5]
+    }
+]
+
+for(let aluno of listaKaratecas){
+    // let media = 0
+    // aluno.notas.forEach(nota => media = media + nota)
+    // media = media / aluno.notas.length
+
+    let media = (aluno.notas[0] + aluno.notas[1]) / 2
+
+    console.log(`Aluno: ${aluno.nome} \nMédia: ${media}`)
+}
+
